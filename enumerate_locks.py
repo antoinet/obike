97559917d3360c7c9229ff90b94f01c11afd9c58
Antoine Neuenschwander <antoine@schoggi.org>
2018-09-16 21:49:30 +0000

    Enumerated locks for DE (049...)

--- enumerate_locks.py
+++ enumerate_locks.py
@@ -3,8 +3,8 @@ from obike.http_client import HttpClient
 client = HttpClient()
 
 f = open('lockdb.txt', 'w')
-for i in range(9999, 11000):
-    bikeid = '041%06d' % i
+for i in range(1000, 9999):
+    bikeid = '049%06d' % i
     lockno = client.lock_no(bikeid)
     print "%s %s" % (bikeid, lockno)
     f.write("%s %s\n" % (bikeid, lockno))
